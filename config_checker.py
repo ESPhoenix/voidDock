51a29a8d838fabba2df6a63826b927471764285a
ESPhoenix <nell@LAPTOP-UL4UA61J>
2024-06-18 12:11:42 +0100

    clean code on config checker

--- config_checker.py
+++ config_checker.py
@@ -8,6 +8,16 @@ import multiprocessing as mp
 
 #########################################################################
 def read_and_validate_config() -> dict:
+    """
+    Main script for config checker:
+    1. Accepts --config flag arg with argpass, reads yaml file into a dict
+    2. Checks paths in pathInfo to see if they are real
+    3. Checks inputs of cpuInfo to see if they are correct
+    4. Checks each dockingOrder in dockingOrders to see if they are correct
+
+    Returns:
+    - config (dict)
+    """
 
     print("checking config file...")
     config: dict = read_input_yaml()
@@ -19,6 +29,13 @@ def read_and_validate_config() -> dict:
 
 #########################################################################
 def read_input_yaml() -> dict:
+    """
+    Reads a YAML file using the "--config" flag with argpass
+    Reads YAML file into a dict
+
+    Returns:
+    - config (dict)
+    """
     # create an argpass parser, read config file,
     parser = argpass.ArgumentParser()
     parser.add_argument("--config")
@@ -31,35 +48,46 @@ def read_input_yaml() -> dict:
             config: dict = yaml.safe_load(yamlFile)
             return config
     except FileNotFoundError:
-        print("Configuration file not found.")
-        exit(1)
+        raise FileNotFoundError(f"config file {configFile} not found")
     except yaml.YAMLError as exc:
-        print("Error parsing YAML file:", exc)
-        exit(1)
+        raise yaml.YAMLError("Error parsing YAML file:", exc)
+
 
 
 #########################################################################
 def check_pathInfo(config: dict) -> None:
-    pathInfo, = check_info_for_manditory_args(config, "config", ["pathInfo"])
-    protDir, ligandDir, outDir = check_info_for_manditory_args(pathInfo, "pathInfo", ["protDir", "ligandDir", "outDir"])
-    for argValue, argName in zip([protDir, ligandDir, outDir], ["protDir", "ligandDir", "outDir"]):
+    """
+    Checks for pathInfo entry in config
+    Checks paths in pathInfo to see if they are real
+    Don't check outDir, this will be made automatically
+    """
+    ## check if pathInfo in config
+    pathInfo, = check_info_for_args(config, "config", ["pathInfo"], optional=False)
+    # check for required args in pathInfo
+    protDir, ligandDir, outDir = check_info_for_args(pathInfo, "pathInfo", ["protDir", "ligandDir", "outDir"], optional=False)
+    ## make sure paths exist
+    for argValue, argName in zip([protDir, ligandDir], ["protDir", "ligandDir"]):
         validate_path(argName, argValue)
 #########################################################################
 
 def check_cpuInfo(config: dict) -> None:
-    cpuInfo, = check_info_for_manditory_args(config,
-                                             "config",
-                                                 ["cpuInfo"])
+    """
+    Checks for cpuInfo in config
+    Checks that entries in cpuInfo are int types
+    Makes sure your computer has enough CPUs for the simulation
+    """
 
-    totalCpuUseage, cpusPerRun = check_info_for_manditory_args(cpuInfo,
-                                                                "cpuInfo",
-                                                                  ["totalCpuUsage", "cpusPerRun"])
+    ## check for cpuInfo in config
+    cpuInfo, = check_info_for_args(config, "config", ["cpuInfo"], optional= False)
+    ## check for required args in cpuInfo
+    totalCpuUseage, cpusPerRun = check_info_for_args(cpuInfo, "cpuInfo", ["totalCpuUsage", "cpusPerRun"], optional= False)
     ## check that cpuInfo arguments are int values and are positive
     for argValue, argName in zip([totalCpuUseage, cpusPerRun], ["totalCpuUseage", "cpusPerRun"]):
         if not isinstance(argValue, int):
             raise TypeError(f"The config argument {argName} = {argValue} is not a an int type.")
         if argValue < 1:
             raise ValueError(f"The config argument {argName} = {argValue} must be a int greater than 1")
+    ## check that your computer has enough CPUs
     if totalCpuUseage > mp.cpu_count():
         raise ValueError("totalCpuUseage argument exceeds your computers number of cores")
 
@@ -67,59 +95,132 @@ def check_cpuInfo(config: dict) -> None:
 
 #########################################################################
 def check_dockingOrders(config: dict) -> None:
+    """
+    Checks for dockingOrders in config, makes sure it's a list and not empty
+    For each entry in dockingOrders:
+    - checks for required args
+    - ensures that input files exist in the expected directories
+    - checks for optional args
+    - ensures that residue dicts in optional args are formatted correctly
+
+    NOTE that this checks basic formatting - if you have specified the
+    wrong residue with the correct format,  this will not ne caught.
+    The docking simulation prep will fail at some point later on!
+    """
+
     ## we know pathInfo is all good, so we can use it here
     pathInfo = config["pathInfo"]
     protDir = pathInfo["protDir"]
     ligandDir = pathInfo["ligandDir"]
-    dockingOrders, = check_info_for_manditory_args(config, "config", ["dockingOrders"])
-
+    ## check for dockingOrders in config
+    dockingOrders, = check_info_for_args(config, "config", ["dockingOrders"], optional= False)
+    ## ensure that dockingOrders is a non-zero-length list
+    if not isinstance(dockingOrders, list):
+        raise TypeError("dockingOrders must be a list containing dicts")
     if len(dockingOrders) == 0:
-        print("No entries in dockingOrders argument in config")
-        exit(1)
+        raise ValueError("dockingOrders must have at least one entry")
+    ## look through each entry in dockingOrders
     for dockingOrder in dockingOrders:
-        ## mandatory args
-        for  argName in ["protein", "ligands", "pocketResidues"]:
-            isArg, argValue = check_dict_for_key(dockingOrder, argName)
-            if not isArg:
-                print(f"Argument {argName} not found in cpuInfo")
-                exit(1)
-        ## check to see if protein and ligand files have pdb files where you would expect them
-        protName = dockingOrder["protein"] 
-        protPdb = p.join(protDir,f"{protName}.pdb")
+        ## check for required args in dockingOrder
+        protName, ligandNames =   check_info_for_args(dockingOrder, "dockingOrder", ["protein", "ligands"], optional= False)
+        ## check to see if protein and ligand files exist in expected directories
+        protPdb: Union[PathLike, str] = p.join(protDir,f"{protName}.pdb")
         if not p.isfile(protPdb):
             raise FileNotFoundError(f"Protein PDB {protPdb} does not exist")
-        ligandNames = dockingOrder["ligands"]
+        ligandNames: list = dockingOrder["ligands"]
         for ligandName in ligandNames:
-            ligandPdb = p.join(ligandDir, f"{ligandName}.pdb")
+            ligandPdb: Union[PathLike, str] = p.join(ligandDir, f"{ligandName}.pdb")
             if not p.isfile(ligandPdb):
                 raise FileNotFoundError(f"Ligand PDB {ligandPdb} does not exist")
-
+        ## check for optional args in dockingOrder
+        pocketResidues, keepResidues, flexibleResidues = check_info_for_args(dockingOrder, "dockingOrder", ["pocketResidues", "keepResidues", "flexibleResidues"], optional=True)
+        ## ensure that these residue-specifying dicts are formatted correctly
+        for residues, dictName in zip([pocketResidues, keepResidues, flexibleResidues],["pocketResidues", "keepResidues", "flexibleResidues"]):
+            validate_residue_dict(residues, dictName)
 #########################################################################
-def check_info_for_manditory_args(info, infoName,  argNames):
-    unpackedDicts = []
+def check_info_for_args(info: dict, infoName: str,  argNames: list, optional: bool) -> list:
+    """
+    Simple check to see if list of keys is in a dict
+    If optional is set to "False", raises a KeyError
+    If all is as expected returns a list of values to be unpacked
+    """
+    ## init empty list to append to 
+    unpackedDicts: list = []
     for  argName in argNames:
         isArg, argValue = check_dict_for_key(info, argName)
-        if not isArg:
+        ## if a required arg is not in the dict, raise a KeyError
+        if not isArg and not optional:
             raise KeyError(f"Argument {argName} not found in {infoName}")
         unpackedDicts.append(argValue)
     return unpackedDicts
 #########################################################################
 def check_dict_for_key(info: dict, key: any) -> Tuple[bool, any]:
+    """
+    Checks to see if a key is in a dict
+    If it is, return "True" and the associated value 
+    """
     if key in info:
         if info[key] == False:
             return True, False
         else:
             return True, info[key]
-    return False, None
+    return False, False
 #########################################################################
-def validate_path(argName, argPath):
+def validate_path(argName: str, argPath: Union[PathLike, str]) -> None:
+    """
+    Check to see if a path variable is indeed the correct type
+    Check to see if the path exists
+    """
     if  not isinstance(argPath, (os.PathLike, str)) :
         raise TypeError(f"The config argument {argName} = {argPath} is not a PathLike.")
     # Check if the path exists
     if not p.exists(argPath):
         raise FileNotFoundError(f"The config argument {argName} = {argPath} does not exist.")
 #########################################################################
-def validate_residue_dicts(info):
+def validate_residue_dict(info: dict, infoName: str) -> None:
+    """
+    Ensures that the residue-specifying dicts are correctly formatted, eg:
+    {"CHAIN_ID": "B", "RES_NAME: "GLY", "RES_ID": 123}
+    """
+    aminoAcidThreeLetterNames = init_amino_acid_list()
     for residue in info:
-        chainId, resName, resId = check_info_for_manditory_args(residue, "residue", ["CHAIN_ID", "RES_NAME", "RES_ID"])
+        chainId, resName, resId = check_info_for_args(residue, "residue", ["CHAIN_ID", "RES_NAME", "RES_ID"], optional=False)
+        if not isinstance(chainId, str):
+            raise TypeError(f"CHAIN_ID in {infoName} must be a string")
+        if not isinstance(resName, str):
+            raise TypeError(f"RES_NAME in {infoName} must be a string")
+        if not resName in aminoAcidThreeLetterNames:
+            raise ValueError(f"RES_NAME in {infoName} must be a canonical amino acid three-letter, ALL-CAPS code")
+        if not isinstance(resId, int):
+            raise TypeError(f"RES_ID in {infoName} must be a int")
+        if resId < 1:
+            raise ValueError(f"RES_ID in {infoName} must be a positive int")
 #########################################################################
+def init_amino_acid_list() -> list:
+    """
+    Creates list of the three-letter codes for the 20 amino acids
+    TODO: add method for dealing with non-naturals
+    """
+    aminoAcidThreeLetterNames: list = [
+        "ALA",  # Alanine
+        "ARG",  # Arginine
+        "ASN",  # Asparagine
+        "ASP",  # Aspartic acid
+        "CYS",  # Cysteine
+        "GLN",  # Glutamine
+        "GLU",  # Glutamic acid
+        "GLY",  # Glycine
+        "HIS",  # Histidine
+        "ILE",  # Isoleucine
+        "LEU",  # Leucine
+        "LYS",  # Lysine
+        "MET",  # Methionine
+        "PHE",  # Phenylalanine
+        "PRO",  # Proline
+        "SER",  # Serine
+        "THR",  # Threonine
+        "TRP",  # Tryptophan
+        "TYR",  # Tyrosine
+        "VAL"   # Valine
+    ]
+    return aminoAcidThreeLetterNames
